6d86458953ac26b18ed2eb1565cf8df9ffb15747
b.lusztak <b.lusztak@student.mini.pw.edu.pl>
2021-02-10 03:11:07 +0100

    Data base first attitude to create models using Entity Framework tools

---
+++ PumoxBackend/PumoxBackend/Models/PumoxContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PumoxBackend.Models
+{
+    public partial class PumoxContext : DbContext
+    {
+        public PumoxContext()
+        {
+        }
+
+        public PumoxContext(DbContextOptions<PumoxContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Company> Companies { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Pumox;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.Property(e => e.EstablishmentYear).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.HasOne(d => d.Company)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.CompanyId)
+                    .HasConstraintName("FK_Employees_Companies");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ database/CreateScript.sql
@@ -0,0 +1,68 @@
+SET NOCOUNT ON
+GO
+
+USE master
+
+GO
+if exists (select * from sysdatabases where name='Pumox')
+		drop database Pumox
+Go
+
+DECLARE @device_directory NVARCHAR(520)
+SELECT @device_directory = SUBSTRING(filename, 1, CHARINDEX(N'master.mdf', LOWER(filename)) - 1)
+FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
+
+EXECUTE (N'CREATE DATABASE Pumox
+  ON PRIMARY (NAME = N''Pumox'', FILENAME = N''' + @device_directory + N'Pumox.mdf'')
+  LOG ON (NAME = N''Pumox_log'',  FILENAME = N''' + @device_directory + N'Pumox.ldf'')')
+go
+
+GO
+
+set quoted_identifier on
+GO
+
+SET DATEFORMAT mdy
+GO
+
+use "Pumox"
+GO
+
+drop table if exists Companies
+drop table if exists Employees
+
+
+GO
+
+CREATE TABLE Companies (
+	Id bigint IDENTITY (1, 1) NOT NULL ,
+	"Name" nvarchar (50) NOT NULL,
+	EstablishmentYear datetime NULL,
+	CONSTRAINT PK_Companies PRIMARY KEY CLUSTERED
+	(
+		Id
+	)
+)
+GO
+
+CREATE TABLE Employees(
+	Id bigint IDENTITY(1, 1) NOT NULL,
+	FirstName nvarchar (30) NOT NULL,
+	LastName  nvarchar (30) NOT NULL,
+	DateOfBirth datetime NULL,
+	JobTitle tinyint NOT NULL,
+	CompanyId bigint NOT NULL,
+	CONSTRAINT PK_Employees PRIMARY KEY CLUSTERED
+	(
+		Id
+	),
+	CONSTRAINT FK_Employees_Companies FOREIGN KEY
+	(
+		CompanyId
+	) REFERENCES Companies
+	(
+		Id
+	) ON DELETE CASCADE
+)
+
+
