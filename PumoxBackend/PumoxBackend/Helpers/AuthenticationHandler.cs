27325af223e445888f810e3baf9d516434a0858e
bartman13 <37373422+bartman13@users.noreply.github.com>
2021-02-15 15:50:50 +0100

    Authorization Added (#3)
    Co-authored-by: b.lusztak <b.lusztak@student.mini.pw.edu.pl>

---
+++ PumoxBackend/PumoxBackend/Helpers/AuthenticationHandler.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PumoxBackend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Web.Providers.Entities;
+
+namespace PumoxBackend.Helpers
+{
+    public class AuthenticationHandler
+    {
+        public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+        {
+            private readonly IUserService _userService;
+
+            public BasicAuthenticationHandler(
+                IOptionsMonitor<AuthenticationSchemeOptions> options,
+                ILoggerFactory logger,
+                UrlEncoder encoder,
+                ISystemClock clock,
+                IUserService userService)
+                : base(options, logger, encoder, clock)
+            {
+                _userService = userService;
+            }
+            protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+            {
+                bool authResult;
+                try
+                {
+                    var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                    var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                    var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                    var username = credentials[0];
+                    var password = credentials[1];
+                    authResult =  _userService.Authenticate(username, password);
+                }
+                catch
+                {
+                    return  Task.Run(() => AuthenticateResult.Fail("Error Occured.Authorization failed."));
+                }
+
+                if (! authResult)
+                    return Task.Run(() => AuthenticateResult.Fail("Invalid Credentials"));
+                var claims = new[]
+                {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "user"),
+                };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                return  Task.Run(() => AuthenticateResult.Success(ticket));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # Pumox_Task
+
+Respository created for interviev execrise for Pumox GmbH Company.
+
+Used components:
+-Ms Sql Server
+-Asp Net Core Api 3.1
+-Linq
+-EntityFramework Core
+-AoutMapper
+-JsonSerialization
+
+Credentials set in appsettings.json
+
+username : "user"
+password: "password"
+
+Examples of Endpoints:
+
+/company/create:
+{
+        "Name": "Moja Testowa Firma",
+        "EstablishmentYear": 1998,
+        "Employees": [
+             {
+                 "FirstName": "Bart",
+                 "LastName": "Luszt",
+                 "DateOfBirth": "1990-01-27",
+                "JobTitle": "Architect"
+            },
+             {
+                 "FirstName": "Roman",
+                 "LastName": "Luszt",
+                 "DateOfBirth": "1972-01-27",
+                 "JobTitle": "Manager"
+            }
+        ]
+}
+
+/company/search:
+{
+    
+        {
+    
+        "Keyword": "a",
+        "EmployeeDateOfBirthFrom": "1998-01-27",
+        "EmployeeDateOfBirthTo": "1998-01-30" ,
+        "EmployeeJobTitles": ["Manager","Architect"]
+	}
+}
+
+/company/update/8
+{
+        "Name": "Moja zmieniona firma",
+        "EstablishmentYear": 1998,
+        "Employees": [
+             {
+                 "FirstName": "Bart",
+                 "LastName": "Luszt",
+                 "DateOfBirth": "1990-01-27",
+                "JobTitle": "Architect"
+            },
+             {
+                 "FirstName": "Roman",
+                 "LastName": "Luszt",
+                 "DateOfBirth": "1972-01-27",
+                 "JobTitle": "Manager"
+            }
+        ]
+}
+
+/company/delete/
+{}
